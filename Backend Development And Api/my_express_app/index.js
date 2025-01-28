3c10b63b669894bd31c5c8e351d97f911ae5a7ca
Salman T <salmannoushad003@gmail.com>
2025-01-28 23:07:56 +0530

    update

--- Backend Development And Api/my_express_app/index.js
+++ Backend Development And Api/my_express_app/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
+require('dotenv').config()
 let app = express();
 const PORT = 3000;
 
+app.use((req,res,next)=>{
+    const msg = `${req.method} ${req.path} - ${req.ip}`
+    console.log(msg)
+    next()
+})
 // Define a route
 app.get('/', (req, res) => {
     const absolutePath = __dirname + '/public/index.html'
@@ -9,9 +15,26 @@ app.get('/', (req, res) => {
     res.sendFile(absolutePath);
 });
 
+app.get('/now',(req,res,next)=>{
+    req.time = new Date().toString();
+    next();
+},function(req,res){
+    res.send({time:req.time})
+})
+
+app.get('/name',(req,res)=>{
+    const first = req.query.first;
+    const last = req.query.last;
+    res.send({first,last})
+})
+
+app.get('/book/:book_id',(req,res,next)=>{
+    const id = req.params.book_id;
+    res.json({book_id : id})
+})
 
 app.get('/json',(req,res)=>{
-    res.json({"Message": "Hello Json"});
+    res.json({"Message": process.env.MESSAGE_STYLE == "uppercase" ? ("Hello Json").toUpperCase() : "Hello Json"});
 })
 app.use("/",express.static(__dirname+'/public'))
 // Start the server
