32a0bc7ffebbb9fac0bc342996480a3ad0ab7bf4
Salman T <salmannoushad003@gmail.com>
2025-02-16 23:04:14 +0530

    updated code

--- Backend Development And Api/magoo_db_app/index.js
+++ Backend Development And Api/magoo_db_app/index.js
@@ -1,14 +1,76 @@
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
 
 app.get("/", (req, res) => {
   res.send("Hello, Express");
 });
+app.use(express.json());
 
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-mongoose.connect("mongodb+srv://salmannoushad003:IDPiPAPc2GFTulEl@backenddb.y9vzm.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDb").then(()=>console.log("mangoose connected"))
+app.get("/api/products", async (req, res) => {
+  try {
+    const listofproducts = await Product.find({});
+    res.status(200).json(listofproducts);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+});
+app.get("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+});
+app.put("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if (!product) {
+      return res.json({ message: "not found " });
+    }
+
+    const updatedProduct = await Product.findById(id);
+    res.json(updatedProduct);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+});
+app.delete("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res.json({ message: "not found " });
+    }
+
+    
+    res.status(200).json({message:"message deleted Succesfully"});
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+});
+
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+  
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://salmannoushad003:IDPiPAPc2GFTulEl@backenddb.y9vzm.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDb"
+  )
+  .then(() => console.log("mangoose connected"));
---
+++ Backend Development And Api/magoo_db_app/models/product.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+
+const ProductSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required: [ true,"please enter the product"]
+
+    },
+    quantity:{
+        type:Number,
+        required: true,
+        default : 0
+    },
+   
+},
+{
+    timestamps : true
+}
+);
+
+const Product = mongoose.model("Product",ProductSchema)
+    module.exports = Product;
