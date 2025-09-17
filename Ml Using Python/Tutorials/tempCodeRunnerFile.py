f85924be98545b5ff1c7afb2579d6a2a3ef205cb
Salman T <salmannoushad003@gmail.com>
2025-09-17 23:57:38 +0530

    learned some lessons

---
+++ Ml Using Python/Tutorials/tempCodeRunnerFile.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+import tensorflow as tf
+
+
+
+
+
+# Load dataset.
+dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv') # training data
+dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv') # testing data
+y_train = dftrain.pop('survived')
+y_eval = dfeval.pop('survived')
+
+print(dftrain.head())
